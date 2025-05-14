d7c83675ef925d53998576e14999c7faa38afa0f
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-05-14 18:20:11 -0500

    feat(carrito-compras): implement shopping cart service with local storage and product availability updates
    feat(catalogo): create catalog service and component with product listing and search functionality
    
    test(catalogo): add unit tests for catalog service to ensure product retrieval and price transformation
    
    feat(crear-pedido): implement order creation service with HTTP POST request handling
    
    test(crear-pedido): add unit tests for order creation service to validate request payload
    
    feat(inventory-socket): implement socket service for real-time inventory updates and connection management
    
    test(inventory-socket): add unit tests for socket service to verify connection and event handling
    
    feat(i18n): add English translations for catalog component
    
    fix(environment): add socket URL to environment configuration for production and development

--- src/app/modules/catalogo/paginas/catalogo/catalogo.component.ts
+++ src/app/modules/catalogo/paginas/catalogo/catalogo.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatCard } from '@angular/material/card';
 import {
   IonButton,
@@ -9,9 +9,19 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  ViewWillEnter,
+  ViewWillLeave,
 } from '@ionic/angular/standalone';
+import { UsuarioService } from 'src/app/modules/auth/servicios/usuario.service';
+import { CarritoComprasService } from 'src/app/modules/carrito-compras/servicios/carrito-compras.service';
+import { CatalogoService } from 'src/app/modules/carrito-compras/servicios/catalogo.service';
+import { InventorySocketServiceService } from 'src/app/modules/carrito-compras/servicios/inventory-socket-service.service';
 import { sharedImports } from 'src/app/shared/otros/shared-imports';
 import { HighlightTextPipe } from 'src/app/shared/pipes/highlight-text.pipe';
+import { Producto } from '../../interfaces/productos.interface';
+import { map, Observable, startWith, Subscription } from 'rxjs';
+import { DinamicSearchService } from 'src/app/shared/services/dinamic-search.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-catalogo',
@@ -31,6 +41,85 @@ import { HighlightTextPipe } from 'src/app/shared/pipes/highlight-text.pipe';
     HighlightTextPipe,
   ],
 })
-export class CatalogoComponent {
-  constructor() {}
+export class CatalogoComponent implements ViewWillEnter, ViewWillLeave {
+  // Variables para el catálogo de productos
+  productos: Producto[] = [];
+  formBusquedaProductos = new FormControl('');
+  filterProductos$?: Observable<Producto[]>;
+  carritoCount?: Observable<string>;
+
+  private subscriptionChangeInventory?: Subscription;
+
+  constructor(
+    private inventorySocketServiceService: InventorySocketServiceService,
+    private carritoComprasService: CarritoComprasService,
+    private usuarioService: UsuarioService,
+    private catalogoService: CatalogoService,
+    private dinamicSearchService: DinamicSearchService,
+    private router: Router,
+  ) {}
+
+  ionViewWillEnter() {
+    this.carritoComprasService.setCurrentClient(this.usuarioService.usuario?.user?.id!);
+    this.carritoCount = this.carritoComprasService.getCartItemCount();
+    this.obtenerProductos();
+    this.connectionChangeInventory();
+  }
+
+  obtenerProductos() {
+    this.catalogoService.obtenerProductos().subscribe(res => {
+      this.productos = res;
+      this.filterProductos();
+    });
+  }
+
+  filterProductos() {
+    this.filterProductos$ = this.formBusquedaProductos.valueChanges.pipe(
+      startWith(''),
+      map(name => this.buscar(name || '')),
+    );
+  }
+
+  buscar(name: string) {
+    if (name) {
+      return this.dinamicSearchService.dynamicSearch(this.productos, name);
+    }
+    return this.productos.slice();
+  }
+
+  connectionChangeInventory() {
+    this.subscriptionChangeInventory =
+      this.inventorySocketServiceService.inventoryChange$.subscribe(event => {
+        if (event) {
+          const { product_id, quantity } = event;
+          const indexProduct = this.productos.findIndex(
+            producto => producto.product_id === product_id,
+          );
+          if (indexProduct !== -1) {
+            this.productos[indexProduct].quantity = quantity;
+          }
+        }
+      });
+  }
+
+  ionViewWillLeave() {
+    if (this.subscriptionChangeInventory) {
+      this.subscriptionChangeInventory.unsubscribe();
+    }
+  }
+
+  irCarritoCompras() {
+    this.router.navigate([`/carrito-compras`]);
+  }
+
+  irDetalleProducto(producto: Producto) {
+    this.catalogoService.productoSeleccionado = producto;
+
+    this.router.navigate(['/carrito-compras', producto.product_id]);
+  }
+
+  agregarAlCarrito(producto: Producto) {
+    this.carritoComprasService.addToCurrentCart(producto);
+    producto.quantity_selected = 0;
+  }
 }
