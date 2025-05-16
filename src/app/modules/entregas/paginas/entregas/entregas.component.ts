7a9e07a42d78f477a0fa4e4b5da3eb8c9fb39dec
andresalarcon <w.alarconc@uniandes.edu.co>
2025-05-15 21:39:57 -0500

    Implementation HU-58

--- src/app/modules/entregas/paginas/entregas/entregas.component.ts
+++ src/app/modules/entregas/paginas/entregas/entregas.component.ts
@@ -1,10 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  ViewWillEnter,
+} from '@ionic/angular/standalone';
+import { sharedImports } from '../../../../shared/otros/shared-imports';
+import { MatCardModule } from '@angular/material/card';
+import { HighlightTextPipe } from '../../../../shared/pipes/highlight-text.pipe';
+import { map, Observable, startWith } from 'rxjs';
+import { DinamicSearchService } from '../../../../shared/services/dinamic-search.service';
+import { LoginService } from '../../../auth/servicios/login.service';
+import { Entrega } from '../../interfaces/entregas.interface';
+import { EntregasService } from '../../servicios/entregas.service';
 
 @Component({
   selector: 'app-entregas',
   templateUrl: './entregas.component.html',
   styleUrls: ['./entregas.component.scss'],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    IonButton,
+    sharedImports,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    MatCardModule,
+    HighlightTextPipe,
+  ],
 })
-export class EntregasComponent {
-  constructor() {}
+export class EntregasComponent implements ViewWillEnter {
+  formBusquedaEntregas = new FormControl('');
+  entregas: Entrega[] = [];
+  filterEntregas$?: Observable<Entrega[]>;
+
+  constructor(
+    private entregasService: EntregasService,
+    private dinamicSearchService: DinamicSearchService,
+    private loginService: LoginService,
+  ) {}
+
+  getDeliveries() {
+    console.log('Entra...');
+    this.entregasService.getDeliveries().subscribe(clientes => {
+      this.entregas = clientes;
+      this.filterEntregas();
+    });
+  }
+
+  filterEntregas() {
+    this.filterEntregas$ = this.formBusquedaEntregas.valueChanges.pipe(
+      startWith(''),
+      map(name => this.buscar(name || '')),
+    );
+  }
+
+  buscar(name: string) {
+    if (name) {
+      return this.dinamicSearchService.dynamicSearch(this.entregas, name);
+    }
+    return this.entregas.slice();
+  }
+
+  cerrarSesion() {
+    this.loginService.cerrarSesion();
+  }
+
+  ionViewWillEnter(): void {
+    this.getDeliveries();
+  }
 }
---
+++ src/app/modules/entregas/servicios/entregas.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Entrega } from '../interfaces/entregas.interface';
+import { map, Observable, tap } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { Producto } from '../interfaces/productos.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EntregasService {
+  private apiUrl = environment.apiUrlCCP;
+  constructor(private http: HttpClient) {}
+
+  getDeliveries(): Observable<Entrega[]> {
+    return this.http.get<any>(`${this.apiUrl}/api/v1/sales/sales`).pipe(
+      map((response: any) => {
+        const entregas: Entrega[] = [];
+        if (response && Array.isArray(response)) {
+          console.log('EesponseArray', response);
+          response.forEach((item: any) => {
+            const items = item.items;
+            console.log('Item', item);
+            if (Array.isArray(item.deliveries)) {
+              item.deliveries.forEach((delivery: any) => {
+                const productos: Producto[] = [];
+                let orders = delivery?.orders;
+                if (Array.isArray(orders)) {
+                  orders.forEach((order: any) => {
+                    const product: Producto = {} as Producto;
+                    const productValue = items.find(
+                      (prod: any) => prod['product']?.product_code === order.product_code);
+                    product.name = productValue['product'].name;
+                    product.price = productValue['product'].price;
+                    product.quantity = productValue.quantity;
+                    product.product_code = productValue['product'].product_code;
+                    product.image = productValue['product'].images[0];
+                    productos.push(product);
+                  });
+                }
+                const entrega: Entrega = {} as Entrega;
+                entrega.guide_number = delivery?.shipping_number || '';
+                entrega.status = item?.status || '';
+                entrega.delivery_date = new Date(delivery?.delivery_date || '')
+                  .toISOString()
+                  .slice(0, 10);
+                entrega.initial_date = new Date(delivery?.created_at || '')
+                  .toISOString()
+                  .slice(0, 10);
+                entrega.phone = item?.client.phone || '';
+                entrega.address = item?.address.line || '';
+                entrega.products = productos;
+                entregas.push(entrega);
+              });
+            }
+          });
+        }
+        console.log('entregas', entregas);
+        return entregas;
+      }),
+    );
+  }
+}
